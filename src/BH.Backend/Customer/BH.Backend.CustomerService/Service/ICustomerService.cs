e3347c33b22d86263a2802b7b78a54a70a65fb4c
parami <paramimodarage@gmail.com>
2023-08-24 23:41:29 +0200

    Added customer service

---
+++ src/BH.Backend/Customer/BH.Backend.CustomerService/Service/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using BH.Backend.Models.Db;
+
+namespace BH.Backend.CustomerService.Service
+{
+    public interface ICustomerService
+    {
+        public IEnumerable<Customer> Get();
+        public Customer Get(Guid id);
+    }
+}
