12ad59b3d8a20461819b304ac6a8546de1eb16b0
parami <paramimodarage@gmail.com>
2023-08-24 21:04:05 +0200

    Added Test project for models

---
+++ src/BH.Backend/Tests/BH.Backend.Models.Tests/Mock/AccountMock.cs
@@ -0,0 +1,32 @@
+﻿using BH.Backend.Models.Db;
+
+namespace BH.Backend.Models.Tests.Mock
+{
+    public class AccountMock
+    {
+        private const string AccountNumber_Valid = "NLABNA";
+
+        private const string AccountNumber_InvalidLength = "NLABN";
+
+        public static Account Account_Valid = new Account()
+        {
+            AccountNumber = AccountNumber_Valid,
+        };
+
+        public static Account Account_Invalid_InvalidAccountNumber = new Account()
+        {
+            AccountNumber = AccountNumber_InvalidLength,
+        };
+
+        public static Account Account_Invalid_EmptyAccountNumber = new Account()
+        {
+            AccountNumber = string.Empty,
+        };
+
+        public static Account Account_Invalid_NegativeBalance = new Account()
+        {
+            AccountNumber = AccountNumber_Valid,
+            CurrentBalance = -6,
+        };
+    }
+}
